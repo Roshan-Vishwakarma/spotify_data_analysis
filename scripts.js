4b832e98817ce13c1ecc0ee8d44ca6f102a92d4f
Roshan-Vishwakarma <110754999+Roshan-Vishwakarma@users.noreply.github.com>
2024-01-07 13:34:44 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Add this JavaScript code to your scripts.js file
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('show');
+  });
+  
